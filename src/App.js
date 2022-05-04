f047784d37ca8d03b712d9e0da0403178b0c9149
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-05-04 15:23:03 +0300

    Project card component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar.js";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Card />
     </>
   );
 }
