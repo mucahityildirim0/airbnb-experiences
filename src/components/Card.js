bab096482eaff82d1720b2a19e1b0aa37f3b8270
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-05-04 15:44:07 +0300

    i ll add props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Card() {
-  return <h1>Replace this</h1>;
+  return (
+    <div className="card">
+      <img src="src/image/image 12.png" className="card--image" />
+      <div className="card--stats">
+        <img src="src/image/Star 1.png" alt="star" className="card--star" />
+        <span>5.0</span>
+        <span>(6)</span>
+        <span>USA</span>
+      </div>
+      <p>Life sessons with Katie Zaferes</p>
+      <p>
+        <span className="bold">From 136$</span> / person
+      </p>
+    </div>
+  );
 }
